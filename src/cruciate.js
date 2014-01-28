88a21ae148539f8e2e5c00edc9384d7f9d8a9397
Michael Hogg <michael.hogg.uk@gmail.com>
2014-01-28 16:54:09 +0000

    Add prefixes for theme properties

--- src/cruciate.js
+++ src/cruciate.js
@@ -3,6 +3,11 @@
     /*-----------------*/
     /*  Themes object  */
     /*-----------------*/
+    
+    /* Prefixes for properties: */
+    /*   t = text colour        */
+    /*   b = background colour  */
+    /*   o = border             */
 
     var themes = {};
     
@@ -16,86 +21,86 @@
         font: 'Monaco',
         
         pageHeader: {
-            reviewTitle:        'hsl( 0,   0%, 100%)',
-            reviewTitleHover:   'hsl(60, 100%,  20%)',
-            reviewState:        'hsl( 0,   0%,  50%)',
-            reviewStateOverdue: 'hsl( 0, 100%,  40%)',
-            reviewMembers:      'hsl( 0,   0%,  50%)'
+            tReviewTitle:        'hsl( 0,   0%, 100%)',
+            bReviewTitleHover:   'hsl(60, 100%,  20%)',
+            tReviewState:        'hsl( 0,   0%,  50%)',
+            tReviewStateOverdue: 'hsl( 0, 100%,  40%)',
+            tReviewMembers:      'hsl( 0,   0%,  50%)'
         },
 
         backgrounds: {
-            main:            'hsl(0, 0%, 0%)',
-            highlightedLine: 'hsl(0, 0%, 25%)'
+            bMain:            'hsl(0, 0%, 0%)',
+            bHighlightedLine: 'hsl(0, 0%, 25%)'
         },
         
-        fileCommentsSeparator: 'hsl(0, 0%, 66%)',
+        bFileCommentsSeparator: 'hsl(0, 0%, 66%)',
         
-        columnVerticalBorder: '1px solid hsl(0, 0%, 38%)',
+        oColumnVerticalBorder: '1px solid hsl(0, 0%, 38%)',
         
         revision: {
-            background: 'hsl(0, 0%, 20%)',
-            text:       'hsl(0, 0%, 63%)'
+            bBackground: 'hsl(0, 0%, 20%)',
+            tText:       'hsl(0, 0%, 63%)'
         },
         
         lineNumbers: {
             normal: {
-                text:       'hsl(0, 0%, 40%)'
+                tText:       'hsl(0, 0%, 40%)'
             },
             old: {
-                text:       'hsl(0, 100%, 50%)',
-                background: 'hsl(0, 100%, 30%)'
+                tText:       'hsl(0, 100%, 50%)',
+                bBackground: 'hsl(0, 100%, 30%)'
             },
             new: {
-                text:       'hsl(120, 100%, 40%)',
-                background: 'hsl(120, 100%, 25%)'
+                tText:       'hsl(120, 100%, 40%)',
+                bBackground: 'hsl(120, 100%, 25%)'
             }
         },
         
-        codeTextDefault: 'hsl(0, 0%, 100%)',
+        tCodeTextDefault: 'hsl(0, 0%, 100%)',
         
         token: {
-            keyword:     'hsl(210, 100%, 80%)',
-            startTag:    'hsl(210, 100%, 80%)',
-            endTag:      'hsl(210, 100%, 80%)',
+            tKeyword:     'hsl(210, 100%, 80%)',
+            tStartTag:    'hsl(210, 100%, 80%)',
+            tEndTag:      'hsl(210, 100%, 80%)',
         
-            string:      'hsl(290, 100%, 80%)',
-            charLiteral: 'hsl(290, 100%, 80%)',
-            numeric:     'hsl(290, 100%, 80%)',
+            tString:      'hsl(290, 100%, 80%)',
+            tCharLiteral: 'hsl(290, 100%, 80%)',
+            tNumeric:     'hsl(290, 100%, 80%)',
         
-            comment:     'hsl(22, 100%, 60%)',
-            todo:        'hsl(22, 100%, 60%)',
+            tComment:     'hsl(22, 100%, 60%)',
+            tTodo:        'hsl(22, 100%, 60%)',
         
-            identifier:  'hsl(45, 100%, 66%)',
-            attrib:      'hsl(45, 100%, 66%)'
+            tIdentifier:  'hsl(45, 100%, 66%)',
+            tAttrib:      'hsl(45, 100%, 66%)'
         },
         
         diff: {
             completeBlock: {
-                oldBackground: 'hsl(  0, 100%, 30%)',
-                newBackground: 'hsl(120, 100%, 25%)'
+                bOldBackground: 'hsl(  0, 100%, 30%)',
+                bNewBackground: 'hsl(120, 100%, 25%)'
             },
             partialBlock: {
                 identical: {
-                    oldBackground: 'hsl(  0, 100%, 12%)',
-                    newBackground: 'hsl(120, 100%, 10%)'
+                    bOldBackground: 'hsl(  0, 100%, 12%)',
+                    bNewBackground: 'hsl(120, 100%, 10%)'
                 },
                 changed: {
-                    oldBackground: 'hsl(  0, 100%, 30%)',
-                    newBackground: 'hsl(120, 100%, 25%)'
+                    bOldBackground: 'hsl(  0, 100%, 30%)',
+                    bNewBackground: 'hsl(120, 100%, 25%)'
                 }
             },
-            nonExistentBlockBackground: 'hsl(0, 0%, 20%)',
-            contextSeparatorBackground: 'hsl(0, 0%, 20%)'
+            bNonExistentBlockBackground: 'hsl(0, 0%, 20%)',
+            bContextSeparatorBackground: 'hsl(0, 0%, 20%)'
         },
         
         userComment: {
-            default:           'hsl(60, 25%, 60%)',
-            read:              'hsl(60, 25%, 60%)',
-            unread:            'hsl(60, 35%, 80%)',
-            leaveUnread:       'hsl(60, 35%, 80%)',
-            readDefect:        'hsl( 0, 25%, 60%)',
-            unreadDefect:      'hsl( 0, 35%, 80%)',
-            leaveUnreadDefect: 'hsl( 0, 35%, 80%)'
+            bDefault:           'hsl(60, 25%, 60%)',
+            bRead:              'hsl(60, 25%, 60%)',
+            bUnread:            'hsl(60, 35%, 80%)',
+            bLeaveUnread:       'hsl(60, 35%, 80%)',
+            bReadDefect:        'hsl( 0, 25%, 60%)',
+            bUnreadDefect:      'hsl( 0, 35%, 80%)',
+            bLeaveUnreadDefect: 'hsl( 0, 35%, 80%)'
         }
 
     };
@@ -118,22 +123,22 @@
     /*----- Page header -----*/
 
     /* Background */
-    "#atlas { background-color: " + theme.backgrounds.main + "; } " +
+    "#atlas { background-color: " + theme.backgrounds.bMain + "; } " +
     
     /* Review title: colour */
-    "#reviewHead              { color: " + theme.pageHeader.reviewTitle + "; } " +
-    "#reviewHead span.title a { color: " + theme.pageHeader.reviewTitle + "; } " +
+    "#reviewHead              { color: " + theme.pageHeader.tReviewTitle + "; } " +
+    "#reviewHead span.title a { color: " + theme.pageHeader.tReviewTitle + "; } " +
 
     /* Review title: background colour */
-    "#reviewHead.editable:hover { background-color: " + theme.pageHeader.reviewTitleHover + "; } " +
+    "#reviewHead.editable:hover { background-color: " + theme.pageHeader.bReviewTitleHover + "; } " +
     
     /* Review state */
-    "#review-state      { color: " + theme.pageHeader.reviewState        + "; } " +
-    "#review-state span { color: " + theme.pageHeader.reviewState        + " !important; } " +
-    "#review-state em   { color: " + theme.pageHeader.reviewStateOverdue + "; } " +
+    "#review-state      { color: " + theme.pageHeader.tReviewState        + "; } " +
+    "#review-state span { color: " + theme.pageHeader.tReviewState        + " !important; } " +
+    "#review-state em   { color: " + theme.pageHeader.tReviewStateOverdue + "; } " +
     
     /* Review members */
-    ".review-members h5 { color: " + theme.pageHeader.reviewMembers + "; } " +
+    ".review-members h5 { color: " + theme.pageHeader.tReviewMembers + "; } " +
     
     /* Progress bar */
     "#status { -webkit-filter: invert(100%); } " +
@@ -142,104 +147,104 @@
     /*----- Backgrounds -----*/
 
     /* Main background (can be seen with very short files which don't fill the screen) */
-    "#frx-pane { background-color: " + theme.backgrounds.main + "; } " +
+    "#frx-pane { background-color: " + theme.backgrounds.bMain + "; } " +
 
     /* Background of file comments container */
-    ".revision_comments_frxinner { background-color: " + theme.backgrounds.main + "; } " +
+    ".revision_comments_frxinner { background-color: " + theme.backgrounds.bMain + "; } " +
 
     /* Background of source code container */
-    ".source { background-color: " + theme.backgrounds.main + "; } " +
+    ".source { background-color: " + theme.backgrounds.bMain + "; } " +
     
     /* Background of a highlighted source code line */
     /* Occurs when: */
     /*   1. Selecting one or more lines to add a user comment */
     /*   2. Hovering over an existing user comment */
-    ".lineHighlighted { background-color: " + theme.backgrounds.highlightedLine + "; } " +
+    ".lineHighlighted { background-color: " + theme.backgrounds.bHighlightedLine + "; } " +
     
 
     /*----- File comments separator -----*/
     
-    ".inline-comment-tip { background-color: " + theme.fileCommentsSeparator + "; } " +
+    ".inline-comment-tip { background-color: " + theme.bFileCommentsSeparator + "; } " +
     
 
     /*----- Columns -----*/
 
     /* Column 1 */
-    ".tetrisColumn { background-color: " + theme.backgrounds.main + "; font-size: 1pt; } " +
+    ".tetrisColumn { background-color: " + theme.backgrounds.bMain + "; font-size: 1pt; } " +
 
     /* Column 2 */
-    ".diffNav     { background-color: " + theme.backgrounds.main + "; border-right: " + theme.columnVerticalBorder + "; font-size: 1pt; } " +
+    ".diffNav     { background-color: " + theme.backgrounds.bMain + "; border-right: " + theme.oColumnVerticalBorder + "; font-size: 1pt; } " +
     ".diffNav img { -webkit-filter: invert(100%); } " +
 
     /* Column 3 */
-    ".inlineSource td.author { background-color: " + theme.backgrounds.main + "; } " +
+    ".inlineSource td.author { background-color: " + theme.backgrounds.bMain + "; } " +
 
     /* Column 4 */
-    "td.revision { background-color: " + theme.revision.background + "; color: " + theme.revision.text + "; } " +
+    "td.revision { background-color: " + theme.revision.bBackground + "; color: " + theme.revision.tText + "; } " +
 
     /* Column 5 */
-    ".diffLineNumbers  { font-family: " + theme.font + "; color: " + theme.lineNumbers.normal.text + "; } " +
-    ".diffLineNumbersA { font-family: " + theme.font + "; color: " + theme.lineNumbers.old.text    + "; background-color: " + theme.lineNumbers.old.background + "; } " +
-    ".diffLineNumbersB { font-family: " + theme.font + "; color: " + theme.lineNumbers.new.text    + "; background-color: " + theme.lineNumbers.new.background + "; } " +
+    ".diffLineNumbers  { font-family: " + theme.font + "; color: " + theme.lineNumbers.normal.tText + "; } " +
+    ".diffLineNumbersA { font-family: " + theme.font + "; color: " + theme.lineNumbers.old.tText    + "; background-color: " + theme.lineNumbers.old.bBackground + "; } " +
+    ".diffLineNumbersB { font-family: " + theme.font + "; color: " + theme.lineNumbers.new.tText    + "; background-color: " + theme.lineNumbers.new.bBackground + "; } " +
 
     /* Column 6 */
-    ".lineContent { font-family: " + theme.font                 + "; color: "        + theme.codeTextDefault      + "; } " +
-    ".diffContent { border-left: " + theme.columnVerticalBorder + "; border-right: " + theme.columnVerticalBorder + "; } " +
+    ".lineContent { font-family: " + theme.font                  + "; color: "        + theme.tCodeTextDefault      + "; } " +
+    ".diffContent { border-left: " + theme.oColumnVerticalBorder + "; border-right: " + theme.oColumnVerticalBorder + "; } " +
 
 
     /*----- Tokens -----*/
 
     /* Keywords and tags */
-    ".hl_keyword  { color: " + theme.token.keyword  + "; font-weight: normal; } " +
-    ".hl_starttag { color: " + theme.token.startTag + "; font-weight: normal; } " +
-    ".hl_endtag   { color: " + theme.token.endTag   + "; font-weight: normal; } " +
+    ".hl_keyword  { color: " + theme.token.tKeyword  + "; font-weight: normal; } " +
+    ".hl_starttag { color: " + theme.token.tStartTag + "; font-weight: normal; } " +
+    ".hl_endtag   { color: " + theme.token.tEndTag   + "; font-weight: normal; } " +
 
     /* Strings, char literals and numeric */
-    ".hl_string       { color: " + theme.token.string      + "; font-weight: normal; } " +
-    ".hl_char_literal { color: " + theme.token.charLiteral + "; font-weight: normal; } " +
-    ".hl_numeric      { color: " + theme.token.numeric     + "; } " +
+    ".hl_string       { color: " + theme.token.tString      + "; font-weight: normal; } " +
+    ".hl_char_literal { color: " + theme.token.tCharLiteral + "; font-weight: normal; } " +
+    ".hl_numeric      { color: " + theme.token.tNumeric     + "; } " +
 
     /* Comments and todo tags */
-    ".hl_comment { color: " + theme.token.comment + "; font-style:  normal; } " +
-    ".hl_todo    { color: " + theme.token.todo    + "; font-weight: normal; } " +
+    ".hl_comment { color: " + theme.token.tComment + "; font-style:  normal; } " +
+    ".hl_todo    { color: " + theme.token.tTodo    + "; font-weight: normal; } " +
 
     /* Other */
-    ".hl_identifier { color: " + theme.token.identifier + "; } " +
-    ".hl_attrib     { color: " + theme.token.attrib     + "; } " +
+    ".hl_identifier { color: " + theme.token.tIdentifier + "; } " +
+    ".hl_attrib     { color: " + theme.token.tAttrib     + "; } " +
 
 
     /*----- Diff -----*/
 
     /* Complete block */
-    "td.diffContentA { background-color: " + theme.diff.completeBlock.oldBackground + "; border-left: " + theme.columnVerticalBorder + "; border-right: " + theme.columnVerticalBorder + "; } " +
-    "td.diffContentB { background-color: " + theme.diff.completeBlock.newBackground + "; border-left: " + theme.columnVerticalBorder + "; border-right: " + theme.columnVerticalBorder + "; } " +
+    "td.diffContentA { background-color: " + theme.diff.completeBlock.bOldBackground + "; border-left: " + theme.oColumnVerticalBorder + "; border-right: " + theme.oColumnVerticalBorder + "; } " +
+    "td.diffContentB { background-color: " + theme.diff.completeBlock.bNewBackground + "; border-left: " + theme.oColumnVerticalBorder + "; border-right: " + theme.oColumnVerticalBorder + "; } " +
 
     /* Partial block: identical */
-    ".ediffContentA { background-color: " + theme.diff.partialBlock.identical.oldBackground + "; border-left: " + theme.columnVerticalBorder + "; border-right: " + theme.columnVerticalBorder + "; } " +
-    ".ediffContentB { background-color: " + theme.diff.partialBlock.identical.newBackground + "; border-left: " + theme.columnVerticalBorder + "; border-right: " + theme.columnVerticalBorder + "; } " +
+    ".ediffContentA { background-color: " + theme.diff.partialBlock.identical.bOldBackground + "; border-left: " + theme.oColumnVerticalBorder + "; border-right: " + theme.oColumnVerticalBorder + "; } " +
+    ".ediffContentB { background-color: " + theme.diff.partialBlock.identical.bNewBackground + "; border-left: " + theme.oColumnVerticalBorder + "; border-right: " + theme.oColumnVerticalBorder + "; } " +
 
     /* Partial block: changed */
-    ".ediffChangedA { background-color: " + theme.diff.partialBlock.changed.oldBackground + "; padding: 1px 0px; } " +
-    ".ediffChangedB { background-color: " + theme.diff.partialBlock.changed.newBackground + "; padding: 1px 0px; } " +
+    ".ediffChangedA { background-color: " + theme.diff.partialBlock.changed.bOldBackground + "; padding: 1px 0px; } " +
+    ".ediffChangedB { background-color: " + theme.diff.partialBlock.changed.bNewBackground + "; padding: 1px 0px; } " +
 
     /* Non-existent block (code doesn't exist in other pane) */
-    ".cells-empty             .cell-empty { background-color: " + theme.diff.nonExistentBlockBackground + "; } " +
-    ".inlineSource   td.author.cell-empty { background-color: " + theme.diff.nonExistentBlockBackground + "; } " +
-    ".inlineSource td.revision.cell-empty { background-color: " + theme.diff.nonExistentBlockBackground + "; } " +
+    ".cells-empty             .cell-empty { background-color: " + theme.diff.bNonExistentBlockBackground + "; } " +
+    ".inlineSource   td.author.cell-empty { background-color: " + theme.diff.bNonExistentBlockBackground + "; } " +
+    ".inlineSource td.revision.cell-empty { background-color: " + theme.diff.bNonExistentBlockBackground + "; } " +
 
     /* Context separator */
-    ".activeFrx .diffSkipped td { background-image: none; background-color: " + theme.diff.contextSeparatorBackground + "; } " +
+    ".activeFrx .diffSkipped td { background-image: none; background-color: " + theme.diff.bContextSeparatorBackground + "; } " +
 
 
     /*----- User comments -----*/
 
-    ".comment                    { border: 0; background-color: " + theme.userComment.default           + "; } " +
-    ".comment.read               { border: 0; background-color: " + theme.userComment.read              + "; } " +
-    ".comment.unread             { border: 0; background-color: " + theme.userComment.unread            + "; } " +
-    ".comment.leaveUnread        { border: 0; background-color: " + theme.userComment.leaveUnread       + "; } " +
-    ".comment.read.defect        { border: 0; background-color: " + theme.userComment.readDefect        + "; } " +
-    ".comment.unread.defect      { border: 0; background-color: " + theme.userComment.unreadDefect      + "; } " +
-    ".comment.leaveUnread.defect { border: 0; background-color: " + theme.userComment.leaveUnreadDefect + "; } ";
+    ".comment                    { border: 0; background-color: " + theme.userComment.bDefault           + "; } " +
+    ".comment.read               { border: 0; background-color: " + theme.userComment.bRead              + "; } " +
+    ".comment.unread             { border: 0; background-color: " + theme.userComment.bUnread            + "; } " +
+    ".comment.leaveUnread        { border: 0; background-color: " + theme.userComment.bLeaveUnread       + "; } " +
+    ".comment.read.defect        { border: 0; background-color: " + theme.userComment.bReadDefect        + "; } " +
+    ".comment.unread.defect      { border: 0; background-color: " + theme.userComment.bUnreadDefect      + "; } " +
+    ".comment.leaveUnread.defect { border: 0; background-color: " + theme.userComment.bLeaveUnreadDefect + "; } ";
 
 
     /*--------------------------------*/
