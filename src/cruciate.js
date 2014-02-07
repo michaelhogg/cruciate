e887bbd65fda110cc86af223070100cc17a0a79e
Michael Hogg <michael.hogg.uk@gmail.com>
2014-02-07 09:56:20 +0000

    Convert comments

--- src/cruciate.js
+++ src/cruciate.js
@@ -4,12 +4,12 @@
     /*  Themes object  */
     /*-----------------*/
     
-    /* Prefixes for properties: */
-    /*   t = text colour        */
-    /*   b = background colour  */
-    /*   o = border             */
-    /*   d = data URI image     */
-    /*   s = size               */
+    // Prefixes for properties:
+    //   t = text colour
+    //   b = background colour
+    //   o = border
+    //   d = data URI image
+    //   s = size
 
     var themes = {};
     
@@ -18,7 +18,7 @@
     /*  Palettes object  */
     /*-------------------*/
     
-    /* Each theme has a palette, which can be used for storing commonly-used colours */
+    // Each theme has a palette, which can be used for storing commonly-used colours
 
     var palettes = {};
     
@@ -80,7 +80,7 @@
             tFileAddedTag:    'hsl(120, 100%, 30%)',
             tFileDeletedName: palettes.dark.fileDeleted,
             tFileDeletedTag:  'hsl(  0, 100%, 40%)',
-            tDropdownMenuFileOutdated: 'hsl(0, 100%, 66%) !important'  /* !important is required because the original style is also !important */
+            tDropdownMenuFileOutdated: 'hsl(0, 100%, 66%) !important'  // !important is required because the original style is also !important
         },
         
         navigation: {
@@ -212,22 +212,22 @@
 
     /*----- Page header -----*/
 
-    /* Review title: colour */
+    // Review title: colour
     "#reviewHead              { color: " + theme.pageHeader.tReviewTitle + "; } " +
     "#reviewHead span.title a { color: " + theme.pageHeader.tReviewTitle + "; } " +
 
-    /* Review title: background colour */
+    // Review title: background colour
     "#reviewHead.editable:hover { background-color: " + theme.pageHeader.bReviewTitleHover + "; } " +
     
-    /* Review state */
+    // Review state
     "#review-state      { color: " + theme.pageHeader.tReviewState        + "; } " +
     "#review-state span { color: " + theme.pageHeader.tReviewState        + " !important; } " +
     "#review-state em   { color: " + theme.pageHeader.tReviewStateOverdue + "; } " +
     
-    /* Review members */
+    // Review members
     ".review-members h5 { color: " + theme.pageHeader.tReviewMembers + "; } " +
     
-    /* Progress bar */
+    // Progress bar
     "#status { -webkit-filter: invert(100%); } " +
 
 
@@ -330,70 +330,70 @@
 
     /*----- Columns -----*/
 
-    /* Column 1 */
+    // Column 1
     ".tetrisColumn { background-color: " + theme.bMainBackground + "; font-size: 1pt; } " +
 
-    /* Column 2 */
+    // Column 2
     ".diffNav     { background-color: " + theme.bMainBackground + "; border-right: " + theme.oColumnVerticalBorder + "; font-size: 1pt; } " +
     ".diffNav img { -webkit-filter: invert(100%); } " +
 
-    /* Column 3 */
+    // Column 3
     ".inlineSource td.author { background-color: " + theme.author.bBackground + "; } " +
 
-    /* Column 4 */
+    // Column 4
     "td.revision { background-color: " + theme.revision.bBackground + "; color: " + theme.revision.tText + "; } " +
 
-    /* Column 5 */
+    // Column 5
     "td.diffLineNumbers  { font-family: " + theme.font + "; color: " + theme.lineNumbers.normalLines.tText + "; background-color: " + theme.bMainBackground                  + "; } " +
     "td.diffLineNumbersA { font-family: " + theme.font + "; color: " + theme.lineNumbers.oldLines.tText    + "; background-color: " + theme.lineNumbers.oldLines.bBackground + "; } " +
     "td.diffLineNumbersB { font-family: " + theme.font + "; color: " + theme.lineNumbers.newLines.tText    + "; background-color: " + theme.lineNumbers.newLines.bBackground + "; } " +
 
-    /* Column 6 */
+    // Column 6
     ".lineContent { font-family: " + theme.font                  + "; color: "        + theme.tCodeTextDefault      + "; } " +
     ".diffContent { border-left: " + theme.oColumnVerticalBorder + "; border-right: " + theme.oColumnVerticalBorder + "; background-color: " + theme.bMainBackground + "; } " +
 
 
     /*----- Tokens -----*/
 
-    /* Keywords and tags */
+    // Keywords and tags
     ".hl_keyword  { color: " + theme.token.tKeyword  + "; font-weight: normal; } " +
     ".hl_starttag { color: " + theme.token.tStartTag + "; font-weight: normal; } " +
     ".hl_endtag   { color: " + theme.token.tEndTag   + "; font-weight: normal; } " +
 
-    /* Strings, char literals and numeric */
+    // Strings, char literals and numeric
     ".hl_string       { color: " + theme.token.tString      + "; font-weight: normal; } " +
     ".hl_char_literal { color: " + theme.token.tCharLiteral + "; font-weight: normal; } " +
     ".hl_numeric      { color: " + theme.token.tNumeric     + "; } " +
 
-    /* Comments and todo tags */
+    // Comments and todo tags
     ".hl_comment { color: " + theme.token.tComment + "; font-style:  normal; } " +
     ".hl_todo    { color: " + theme.token.tTodo    + "; font-weight: normal; } " +
 
-    /* Other */
+    // Other
     ".hl_identifier { color: " + theme.token.tIdentifier + "; } " +
     ".hl_attrib     { color: " + theme.token.tAttrib     + "; } " +
 
 
     /*----- Diff -----*/
 
-    /* Complete block */
+    // Complete block
     "table td.lineContent.diffContentA { background-color: " + theme.diff.completeBlock.bOldBackground + "; border-left: " + theme.diff.verticalBorder.oOld + "; border-right: " + theme.diff.verticalBorder.oOld + "; } " +
     "table td.lineContent.diffContentB { background-color: " + theme.diff.completeBlock.bNewBackground + "; border-left: " + theme.diff.verticalBorder.oNew + "; border-right: " + theme.diff.verticalBorder.oNew + "; } " +
 
-    /* Partial block: identical */
+    // Partial block: identical
     "table td.lineContent.ediffContentA { background-color: " + theme.diff.partialBlock.identical.bOldBackground + "; border-left: " + theme.diff.verticalBorder.oOld + "; border-right: " + theme.diff.verticalBorder.oOld + "; } " +
     "table td.lineContent.ediffContentB { background-color: " + theme.diff.partialBlock.identical.bNewBackground + "; border-left: " + theme.diff.verticalBorder.oNew + "; border-right: " + theme.diff.verticalBorder.oNew + "; } " +
 
-    /* Partial block: changed */
+    // Partial block: changed
     ".ediffChangedA { background-color: " + theme.diff.partialBlock.changed.bOldBackground + "; padding: 1px 0px; } " +
     ".ediffChangedB { background-color: " + theme.diff.partialBlock.changed.bNewBackground + "; padding: 1px 0px; } " +
 
-    /* Non-existent block (code doesn't exist in other pane) */
+    // Non-existent block (code doesn't exist in other pane)
     ".cells-empty             .cell-empty { background-color: " + theme.bMainBackground      + "; } " +
     ".inlineSource   td.author.cell-empty { background-color: " + theme.author.bBackground   + "; } " +
     ".inlineSource td.revision.cell-empty { background-color: " + theme.revision.bBackground + "; } " +
 
-    /* Context separator */
+    // Context separator
     ".activeFrx .diffSkipped    { border: 0; } " +
     ".activeFrx .diffSkipped td { background: " + theme.diff.contextSeparator.dBackground + "; height: " + theme.diff.contextSeparator.sHeight + "; } " +
 
